c95f85922ad2ef6fadfbe54795e0390145e29b53
João Lee <joaumlimaum@gmail.com>
2025-02-18 14:00:15 -0300

    feat: add shadcn initial setup and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Button>buy</Button>
+      <Input />
+    </>
+  );
 }
